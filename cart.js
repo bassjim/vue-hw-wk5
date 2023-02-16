9bf141a051faeb5f79499e33f0d44eb233c3f4a8
bassjim <106503203+bassjim@users.noreply.github.com>
2023-02-16 11:14:45 +0800

    restart

--- cart.js
+++ cart.js
@@ -1,113 +1,30 @@
-import { vForm, vField ,ErrorMessage } from 'vee-validate';
-const apiUri = 'https://hexschool.github.io/v2/';
+
+const apiUri = 'https://vue3-course-api.hexschool.io/v2';
 const apiPath = 'bassjim';
 
-const productModal = {
-    props:['id'],
-    data(){
-        return{
-            model:{},
-            tempProduct:{},
-        };
-    },
-    template:'#userProductModal',
-    watch:{
-        id(){
-            console.log('productModal',this.id);
-            if(this.id){
-                const url = `${this.apiUri}api/${this.apiPath}/admin/Products/${this.id}`;
-                console.log('productModal',this.id);
-                this.modal = new bootstrap.Modal(this.$ref.Modal);
-                this.modal.shows();
-            }
-            
-        }
-    },
-    methods:{
-        hide(){
-            this.modal.hide()
-        }
-    },
 
-};
 
 
 const app = Vue.createApp({
     data(){
         return{
-            modal:{},
-            tempProduct:{},
-            productId:"",
-            qty:1,
-            cart:{},
-            loadingItem:"",
-            product:[]
+            products: [],
         }
     },
     methods: {
         getProducts(){
-            const url = `${this.apiUri}api/${this.apiPath}/admin/Products/all`;
+            const url = `${this.apiUri}api/${this.apiPath}/products/all`;
             axios.get(`${url}`)
-            .then((res)=>{
-                this.products = res.data.products;
-                console.log(res);
-            })
-            .catch(err=>{
-                alert(err.response.data.message);
+            .then(res =>{
+                console.log('產品列表',res.data.products);
+   
             })
         },
-        openModal(id){
-            this.productId = id;
 
-        },
-        addToCarts(product_id,qty = 1){
-            const url = `${this.apiUri}api/${this.apiPath}/admin/Products/all`;
-            const data = {
-                product_id,
-                qty,
-            }
-            axios.get(`${this.apiUri}api/${this.apiPath}/admin/cart/`,{data})
-            .then(res => {
-                console.log(res);
-                this.$ref.productModal;
-            })
-            .catch(err => {
-                console.error(err); 
-            })
-        },
-        getCarts(){
-            axios.get(`${this.apiUri}api/${this.apiPath}/cart`,)
-            .then((res)=>{
-                this.products = res.data.products;
-                console.log('購物車',res);
-            })
-            .catch(err=>{
-                alert(err.response.data.message);
-            })
-    
-        },
-        updatedCartItem(id) {
-            const data = {
-                product_id:Item.product.id,
-                qty:Item.qty,
-            };
-            axios.put(`${this.apiUri}api/${this.apiPath}/cart`,)
-            .then((res)=>{
-                console.log(res);
-            })
-            .catch(err=>{
-                alert(err.response.data.message);
-            })
-        },
-    },
-    components:{
-        productModal,
-        vForm,
-        vField,
-        ErrorMessage
     },
     mounted(){
-        this.getProducts
+        this.getProducts();
     }
+
 });
-app.mount('#app');
+app.mount('#app')
